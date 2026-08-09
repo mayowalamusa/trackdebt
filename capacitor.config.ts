9b1dc739db4701bc8ed723bcf574af21a5e1e4d3
mayowalamusa <mayowalamusa@gmail.com>
2026-08-09 13:35:55 +0100

    chore: set capacitor webDir to client build output

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.trackdebt.app',
   appName: 'Track Debt',
-  webDir: 'dist'
+  webDir: 'dist/capacitor'
 };
 
 export default config;
