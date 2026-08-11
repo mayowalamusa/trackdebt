4479bf281a82f14871094c3b0eb4a2b660bbfa80
mayowalamusa <mayowalamusa@gmail.com>
2026-08-11 17:20:55 +0100

    txt keyboard

--- android/app/src/main/java/com/trackdebt/app/MainActivity.java
+++ android/app/src/main/java/com/trackdebt/app/MainActivity.java
@@ -1,8 +1,6 @@
 package com.trackdebt.app;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.webkit.WebView;
 import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
@@ -11,27 +9,5 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-        
-        // Ensure the WebView is the root content view and properly focused
-        if (this.bridge != null && this.bridge.getWebView() != null) {
-            WebView webView = this.bridge.getWebView();
-            ViewGroup parent = (ViewGroup) webView.getParent();
-            if (parent != null) {
-                parent.removeView(webView);
-            }
-            setContentView(webView);
-            
-            webView.setFocusable(true);
-            webView.setFocusableInTouchMode(true);
-            webView.requestFocus();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (this.bridge != null && this.bridge.getWebView() != null) {
-            this.bridge.getWebView().requestFocus();
-        }
     }
 }
